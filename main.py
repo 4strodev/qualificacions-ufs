adb6ecd704810df324cbf728166704e539f5c70c
4strodev <jumarurex@gmail.com>
2022-09-21 19:27:18 +0200

    extract methods

--- main.py
+++ main.py
@@ -2,22 +2,28 @@
 from ModulProfessional import ModulProfessional
 from UnitatFormativa import UnitatFormativa
 
+
+def inicialitzar_modul_profesional():
+    global uf1, uf2, uf3, uf1
+    uf1 = UnitatFormativa("UF1. Desenvolupament del programari", 20)
+    uf2 = UnitatFormativa("UF2. Optimització del programari", 20)
+    uf3 = UnitatFormativa("UF3. Introducció al Disseny Orientat a Objectes", 26)
+    uf1.qualificacio = 8
+    uf2.qualificacio = 10
+    uf3.qualificacio = 4
+    global mp5
+    mp5 = ModulProfessional("MP05. Entorns de desenvolupament")
+    mp5.afegir_unitat_formativa(uf1)
+    mp5.afegir_unitat_formativa(uf2)
+    mp5.afegir_unitat_formativa(uf3)
+
+def mostrar_modul_profesional():
+    print(uf1.nom, ":", uf1.qualificacio)
+    print(uf2.nom, ":", uf2.qualificacio)
+    print(uf3.nom, ":", uf3.qualificacio)
+    print(mp5.nom, ":", mp5.get_qualificacio())
+
 # Inici del programa
-uf1 = UnitatFormativa("UF1. Desenvolupament del programari", 20)
-uf2 = UnitatFormativa("UF2. Optimització del programari", 20)
-uf3 = UnitatFormativa("UF3. Introducció al Disseny Orientat a Objectes", 26)
+inicialitzar_modul_profesional()
 
-uf1.qualificacio = 8
-uf2.qualificacio = 10
-uf3.qualificacio = 4
-
-mp5 = ModulProfessional("MP05. Entorns de desenvolupament")
-
-mp5.afegir_unitat_formativa(uf1)
-mp5.afegir_unitat_formativa(uf2)
-mp5.afegir_unitat_formativa(uf3)
-
-print(uf1.nom, ":", uf1.qualificacio)
-print(uf2.nom, ":", uf2.qualificacio)
-print(uf3.nom, ":", uf3.qualificacio)
-print(mp5.nom, ":", mp5.get_qualificacio())
+mostrar_modul_profesional()
